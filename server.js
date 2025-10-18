eff4ea9a10467cbfea3632086652e3aa3c05e070
smartyboiz1 <Smartyboiz.mania@gmail.com>
2025-10-18 22:47:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import path from "path";
 import generate from "./api/suggest.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT ||
+5000;
 
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.json());
 
 app.post("/api/suggest", generate);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
